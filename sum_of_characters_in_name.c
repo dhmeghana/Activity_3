c586bf7ad089e2bb35a0d82bf71082f28eefbb14
Meghana DH <46954351+dhmeghana@users.noreply.github.com>
2022-05-05 12:32:46 +0530

    Add files via upload

---
+++ sum_of_characters_in_name.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+    char *ptr;
+    ptr=(char*)malloc(20*sizeof(char));
+
+    printf("Enter a string: ");
+    scanf("%[^\n]s",ptr);
+
+    printf("%X\n",sum(ptr));
+    free(ptr);
+    return 0;
+}
+int sum(char *Str)
+{
+    int S=0;
+int len = strlen(Str);
+    for (int i= 0;i<len; i++)
+    {
+        S=S+Str[i];
+    }
+return S;
+}
